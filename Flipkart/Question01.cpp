c65c9be433d5ac3c00059af2949b15998c90b871
Pdeo55 <88934417+Pdeo55@users.noreply.github.com>
2023-01-18 14:39:33 +0530

    Initial commit

---
+++ Flipkart/Question01.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+
+// Partition to K Equal Sum Subsets
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ Microsoft/Question01.cpp
@@ -0,0 +1,3 @@
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/
